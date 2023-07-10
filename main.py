ea1c0ba4c30fef24ed074ccc8e0356c9cc70b8cd
Suellyn Gomes <130364166+sxellyn@users.noreply.github.com>
2023-07-10 09:41:27 -0300

    updating
    2 graphics

--- main.py
+++ main.py
@@ -24,17 +24,18 @@ data2 = yf.download(ticker_symbol2, start='2021-01-01', end='2021-12-31')
 
 #exibir os dados (aka dataframe):
 st.subheader("Historico")
-st.dataframe(data)
+st.dataframe(data1)
+st.dataframe(data2)
 
 #exibir grafico 1:
 fig1 = go.Figure()
-fig1.add_trace(go.Scatter(x=data.index, y=data1["Close"], name="Fechamento"))
+fig1.add_trace(go.Scatter(x=data1.index, y=data1["Close"], name="Fechamento"))
 fig1.update_layout(title=f"{ticker_symbol1}", xaxis_title="DATA", yaxis_title="PREÇO")
 st.plotly_chart(fig1)
 
 #exibir grafico 2:
 fig2 = go.Figure()
-fig2.add_trace(go.Scatter(x=data.index, y=data2["Close"], name="Fechamento"))
+fig2.add_trace(go.Scatter(x=data2.index, y=data2["Close"], name="Fechamento"))
 fig2.update_layout(title=f"{ticker_symbol2}", xaxis_title="DATA", yaxis_title="PREÇO")
 st.plotly_chart(fig2)
 
