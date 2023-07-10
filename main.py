af46224581d55a8084512c2ae9ae657912746e0a
Suellyn Gomes <130364166+sxellyn@users.noreply.github.com>
2023-07-10 09:36:27 -0300

    updating

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.title("APPZINHO")
 #barra lateral com opções:
 st.sidebar.title("IRF")
 ticker_symbol1 = st.sidebar.text_input("Nome 1", "AAPL", max_chars=10)
-ticker_symbol2 = st.sidebar.text_input("Nome 2", "AAPL", max_chars=10)
+ticker_symbol2 = st.sidebar.text_input("Nome 2", "MSFT", max_chars=10)
 
 #BAIXAR os dados:
 data = yf.download([ticker_symbol1, ticker_symbol2], start='2021-01-01', end='2021-12-31')
