74d9a38ca388e02b2db67b5483ec293d425e66cf
Suellyn Gomes <130364166+sxellyn@users.noreply.github.com>
2023-07-10 09:20:55 -0300

    updating for 2 graphics

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.title("APPZINHO")
 #barra lateral com opções:
 st.sidebar.title("IRF")
 ticker_symbol1 = st.sidebar.text_input("Nome 1", "AAPL", max_chars=10)
-ticker_symbol2 = st.sidebar.text_input("Nome 2", "MSFT", max_chars=10)
+ticker_symbol2 = st.sidebar.text_input("Nome 2", "AAPL", max_chars=10)
 
 #BAIXAR os dados:
 data = yf.download([ticker_symbol1, ticker_symbol2], start='2021-01-01', end='2021-12-31')
@@ -26,16 +26,16 @@ st.subheader("Historico")
 st.dataframe(data)
 
 #exibir grafico 1:
-fig = go.Figure()
-fig.add_trace(go.Scatter(x = data.index, y = data["Close"], name = "Fechamento"))
-fig.update_layout(title = f"{ticker_symbol1}", xaxis_title = "DATA", yaxis_title = "PREÇO")
-st.plotly_chart(fig)
+fig1 = go.Figure()
+fig1.add_trace(go.Scatter(x=data.index, y=data[ticker_symbol1]["Close"], name="Fechamento"))
+fig1.update_layout(title=f"{ticker_symbol1}", xaxis_title="DATA", yaxis_title="PREÇO")
+st.plotly_chart(fig1)
 
 #exibir grafico 2:
-fig = go.Figure()
-fig.add_trace(go.Scatter(x = data.index, y = data["Close"], name = "Fechamento"))
-fig.update_layout(title = f"{ticker_symbol2}", xaxis_title = "DATA", yaxis_title = "PREÇO")
-st.plotly_chart(fig)
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x=data.index, y=data[ticker_symbol2]["Close"], name="Fechamento"))
+fig2.update_layout(title=f"{ticker_symbol2}", xaxis_title="DATA", yaxis_title="PREÇO")
+st.plotly_chart(fig2)
 
 #derrubar aplicação =  ctrl Z
 
